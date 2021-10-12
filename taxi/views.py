eef4618e70fafec672017a9f38edbdce82479ebd
sandeep <sandeepkp601@gmail.com>
2021-10-12 10:50:14 +0530

    first

--- taxi/views.py
+++ taxi/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 from rest_framework.authentication import SessionAuthentication,BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
@@ -20,6 +21,7 @@ class DriverRegister(viewsets.ModelViewSet):
 class PaymentDetail(viewsets.ModelViewSet):
     queryset=Payment.objects.all()
     serializer_class=PaymentSerialiser
+
 class BookingVehicle(APIView):
     
     def post(self,request):
