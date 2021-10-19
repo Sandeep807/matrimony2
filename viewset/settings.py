15accb03013c35a03a702ef7233414b335cc1464
Sandeep kumar <61458091+Sandeep807@users.noreply.github.com>
2021-10-19 22:08:01 +0530

    Update settings.py

--- viewset/settings.py
+++ viewset/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL='app.Registration'
