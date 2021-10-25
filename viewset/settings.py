b7e64b598c6399271f046af2cf8b41f5e458df16
Sandeep kumar <61458091+Sandeep807@users.noreply.github.com>
2021-10-25 18:58:20 +0530

    Update settings.py

--- viewset/settings.py
+++ viewset/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*6qfjymbq5_y513es%*(q3o@r$!wp#6l3)2c!h$)4ljx-tvpk$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
